c25cd88051135a29fef195a6d7721d39c8d27fb9
koge <koge.k@dentsudigital.co.jp>
2019-06-10 17:07:39 +0900

    001

--- push-registration-master/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'appsignature' => 'none', 
-	'clientid' => getenv('APP_CENTER_API_INTEGRATION_CLIENTID'),
-	'clientsecret' => getenv('APP_CENTER_API_INTEGRATION_CLIENT_SECRET'),
-	'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl',
-    'xmlloc' => './ExactTargetWSDL.xml',
-);
